f1880d65d56255d16e0591d9f65624a3e7943a62
David Ryan <drsaltmills@gmail.com>
2015-02-13 09:52:43 +0000

    Add rower and get rowers functionality complete

--- js/controllers/rowers.js
+++ js/controllers/rowers.js
@@ -1,13 +1,14 @@
 myApp.controller('RowersController', 
 	function($scope, $firebase){
 	var FIREBASE_URL = "https://rowint-trainingtrack.firebaseio.com/"
+	https://rowint-trainingtrack.firebaseio.com
 	var ref = new Firebase(FIREBASE_URL + 'rowers');
 	var rowers = $firebase(ref);
 	$scope.rowers = rowers.$asObject();
 
 
 	$scope.addRower = function(){
-		rowers$push({
+		rowers.$push({
 			name: $scope.rowername,
 			weight: $scope.rowerweight,
 			age: $scope.rowerage,
